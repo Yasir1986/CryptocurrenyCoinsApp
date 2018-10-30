e7b961c5c01a369b22c96d95f80b81a5b18ea374
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-10-30 15:37:46 +0200

    Update Container.js
    fixes

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -3,32 +3,13 @@ import Cards from '../Components/Cards.js';
 
 class Container extends Component {
 
-    // constructor(props) {
-    //     super(props);
-    // }
-
-
 
     render(){ 
-    //     const card = this.state.mainData.map((element) =>
-    //     <Cards mainData = {element} key = {element.id} />
-     
-    // );
-    //     return(
-    //             {this.state.mainData.map((element) =>}
-    //             <div className="div">
-    //                     <ul className="card">{card} </ul>
-    //            </div> 
-    //     );
-    // } 
+  
     return(
         <div className="Main">
         {this.props.coins.map(coin => 
            <Cards key={coin.id} myCoin = {coin} />
-           // <div key={coin.id}>
-            // {coin.name}
-            // </div>
-
         )}
         </div>
     );
